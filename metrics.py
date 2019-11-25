e4b93aa06c3acd9808c97e6eaea9a4d4bcee8f0c
chen <nocater@163.com>
2019-11-25 12:39:13 +0800

    micro_f1

--- metrics.py
+++ metrics.py
@@ -15,18 +15,30 @@ def masked_softmax_cross_entropy(preds, labels, mask, multi_label=True):
 
 def masked_accuracy(preds, labels, mask, multi_label=True):
     """Accuracy with masking."""
-    accuracy_all = None
     if not multi_label:
         correct_prediction = tf.equal(tf.argmax(preds, 1), tf.argmax(labels, 1))
+        accuracy_all = tf.cast(correct_prediction, tf.float32)
+        mask = tf.cast(mask, dtype=tf.float32)
+        mask /= tf.reduce_mean(mask)
+        accuracy_all *= mask
+        return tf.reduce_mean(accuracy_all)
     else:
         # pass
-        preds = tf.where(preds >= 0.5, tf.ones(tf.shape(preds)), tf.zeros(tf.shape(preds)))
-        correct_prediction = tf.equal(preds, labels)
-        correct_prediction = tf.cast(correct_prediction, tf.float32)
-        correct_prediction = tf.reduce_mean(correct_prediction)
+        predictions = tf.where(preds > 0.5, tf.ones(tf.shape(preds)), tf.zeros(tf.shape(preds)))
+        t = tf.multiply(predictions, labels)
+        true_positives = tf.reduce_sum(t, axis=0)
+        predicted_positives = tf.reduce_sum(predictions, axis=0)
+        possible_positives = tf.reduce_sum(labels, axis=0)
 
-    accuracy_all = tf.cast(correct_prediction, tf.float32)
-    mask = tf.cast(mask, dtype=tf.float32)
-    mask /= tf.reduce_mean(mask)
-    accuracy_all *= mask
-    return tf.reduce_mean(accuracy_all)
+        # Macro_F1 metric.
+        precision = true_positives / (predicted_positives + 1e-8)
+        recall = true_positives / (possible_positives + 1e-8)
+        macro_f1 = tf.reduce_mean(2 * precision * recall / (precision + recall + 1e-8))
+
+        # Micro_F1 metric.
+        prec = tf.reduce_sum(true_positives) / tf.reduce_sum(predicted_positives)
+        reca = tf.reduce_sum(true_positives) / tf.reduce_sum(possible_positives)
+        micro_f1 = 2 * prec * reca / (prec + reca + 1e-8)
+        return micro_f1# , macro_f1
+
+
